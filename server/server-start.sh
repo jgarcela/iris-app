436392955a19177619188d3a56ebb4397a9b5f60
jgarcela <jgarcela@pa.uc3m.es>
2026-07-07 13:55:36 +0000

    Redirect web and API startup logs to the shared logs directory.

--- server/server-start.sh
+++ server/server-start.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-nohup python3.11 -m web > web.log 2>&1 &
-nohup python3.11 -m api > api.log 2>&1 &
+nohup python3.11 -m web > ../logs/web.log 2>&1 &
+nohup python3.11 -m api > ../logs/api.log 2>&1 &
